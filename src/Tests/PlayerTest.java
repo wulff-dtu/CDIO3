9ba489a2d8fcd61edc41c230d6919ed6cbb5a998
wulff-dtu <s185036@student.dtu.dk>
2018-11-23 09:36:01 +0100

    PlayerTest

---
+++ src/Tests/PlayerTest.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import Game.Player;
+
+class PlayerTest {
+
+    @org.junit.jupiter.api.Test
+    void changePosition() {
+        //Tests if the player can move forward.
+        Player player1 = new Player("player1");
+        player1.changePosition(3);
+        assertEquals(3, player1.getPosition());
+
+        //Tests if the player can move forward, and the move past the last field of the game (index = 23),
+        //and land on the correct field.
+        Player player2 = new Player("player2");
+        player2.changePosition(20);
+        player2.changePosition(4);
+        assertEquals(0, player2.getPosition());
+
+        //Tests if a player can move backwards.
+        Player player3 = new Player("player3");
+        player3.changePosition(6);
+        player3.changePosition(-2);
+        assertEquals(4, player3.getPosition());
+
+
+        //Tests if a player can move backwards, past the starting field and land on the correct field.
+        Player player4 = new Player("player4");
+        player4.changePosition(2);
+        player4.changePosition(-3);
+        assertEquals(23, player4.getPosition());
+    }
+}
