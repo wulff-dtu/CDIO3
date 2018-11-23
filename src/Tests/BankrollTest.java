883c7117a7bc9d3ee667f75f6f626a46fe8bf45d
Mads <hemermads@gmail.com>
2018-11-23 11:56:21 +0100

    Bankroll test af Malte og Mads viser blackboxtest af bankroll klassens changeBankroll metode, der skal kunne ændre den aktuelle pengebeholdning

---
+++ src/Tests/BankrollTest.java
@@ -0,0 +1,58 @@
+package Tests;
+
+import Game.Bankroll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankrollTest {
+
+
+
+    @Test
+    void changeBalance() {
+        Bankroll testBankroll = new Bankroll();
+        testBankroll.changeBalance(Integer.MIN_VALUE);
+        assertEquals(Integer.MIN_VALUE+0,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(Integer.MIN_VALUE -1);
+        assertEquals(Integer.MIN_VALUE-1,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(Integer.MIN_VALUE+1);
+        assertEquals(Integer.MIN_VALUE+1,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(0-1);
+        assertEquals(0-1,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(0);
+        assertEquals(0,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(0+1);
+        assertEquals(0+1,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(Integer.MAX_VALUE);
+        assertEquals(Integer.MAX_VALUE+0,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(Integer.MAX_VALUE-1);
+        assertEquals(Integer.MAX_VALUE-1,testBankroll.getBalance());
+
+        testBankroll = new Bankroll();
+        testBankroll.changeBalance(Integer.MAX_VALUE+1);
+        assertEquals(Integer.MAX_VALUE+1,testBankroll.getBalance());
+
+
+    }
+
+    @Test
+    void Bankroll(){
+        PlayerDriver testPlayer = new PlayerDriver();
+    }
+
+}
