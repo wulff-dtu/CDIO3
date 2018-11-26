7b272fff180a395cd9765bcb8890af107a79443d
wulff-dtu <s185036@student.dtu.dk>
2018-11-26 09:15:22 +0100

    Ændret Tile tilbage til den gamle version.

--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -2,27 +2,57 @@ package Game;
 
 public class Tile {
 
-    private String title;
+    private String type, title, subtext, description;
+    private int group, price;
+    private int ownerIndex; // indicates the index of the player who owns the tile (if any)
 
-    public Tile(String title) {
+    public Tile(String type, int group, String title, String subtext, String description, int price) {
+        this.type = type;
+        this.group = group;
         this.title = title;
+        this.subtext = subtext;
+        this.description = description;
+        this.price = price;
+        this.ownerIndex = -1; //-1 indicates lack of ownership
+    }
+
+    public void setOwnerIndex(int ownerIndex) {
+        this.ownerIndex = ownerIndex;
+    }
+
+    public int getOwnerIndex () {
+        return ownerIndex;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public int getEffectOnBalance() {
-        return 0;
+    public String getType() {
+        return type;
+    }
+
+    public String getSubtext() {
+        return subtext;
     }
 
     public int getGroup() {
-        return 0;
+        return group;
     }
 
-    public int getOwnerIndex() {
-        return 0;
+    public String getDescription() {
+        return description;
     }
 
-    public void setOwnerIndex(int i) {}
+    public int getPrice() {
+        return price;
+    }
+
+    public String priceToString() {
+        if (price == 0) {
+            return "";
+        } else {
+            return "" + price;
+        }
+    }
 }
